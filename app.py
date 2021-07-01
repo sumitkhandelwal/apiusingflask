be851811ccfc09228de3f4a68c068334c8c0ce4f
Sumit A Khandelwal <sumit3khandelwal@gmail.com>
2021-07-01 12:36:29 +0530

    Upload source code

---
+++ app.py
@@ -0,0 +1,6 @@
+from flask import Flask
+UPLOAD_FOLDER = 'Upload'
+app = Flask(__name__)
+#app.secret_key = "secret key"
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+#app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
